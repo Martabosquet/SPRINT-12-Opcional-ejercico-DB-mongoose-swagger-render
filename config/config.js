e56f027218d0446c9e80b2307624bba9aa01041e
Marta Bosquet <martabosquetlauci@gmail.com>
2026-07-06 15:13:52 +0200

    arreglos

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
-import "dotenv/config";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './env/.env' });
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Base de datos conectada con éxito');
     } catch (error) {
         console.error(error);
